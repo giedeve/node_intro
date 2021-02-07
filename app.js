f6e628ec4881be06b20f4c06fbcd625a94cb4eb6
giedeve <gieczarny@gmail.com>
2021-02-07 15:24:20 +0100

    add const port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const functions = require("./function")
 const express = require("express");
 const path = require("path");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.set("view engine","hbs")
